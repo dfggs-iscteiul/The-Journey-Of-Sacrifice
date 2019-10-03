045de9f88db93cd3efd3df2ea628416f062fbd10
Daniel Gonçalves <dfggs@iscte-iul.pt>
2019-10-03 13:43:28 +0100

    Movement#1
    Script HeroMovement permite que o herói se desloque usando as techas WASD.

---
+++ The-Journey-Of-Sacrifice/Assets/Scripts/HeroMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rigidbody;
+    Vector3 change;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        move();
+    }
+
+    void move()
+    {
+        rigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
+    }
+}
