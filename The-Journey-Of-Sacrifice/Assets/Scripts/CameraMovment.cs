02ba7b1bf8e8c7b5c85b859585c1e766c2826832
Daniel Gonçalves <dfggs@iscte-iul.pt>
2019-10-22 14:59:57 +0100

    Camera #1
    Adicionado movimento smooth à camera e respetivas Boundaries

---
+++ The-Journey-Of-Sacrifice/Assets/Scripts/CameraMovment.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovment : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float y = Mathf.Clamp(target.position.y, (float)(-3.6), (float)(5.71));
+        float x = Mathf.Clamp(target.position.x, (float)(-4.1), (float)(4.1));
+
+        if (transform.position!=target.position)
+        {
+            Vector3 targetPosition = new Vector3(x, y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
+    }
+}
