4ba904c12ef051b9f4a756de7190e2f04829d3bb
Daniel Gonçalves <dfggs@iscte-iul.pt>
2019-11-14 00:15:48 +0000

    Scenes #1
    Added new scene for the first boss and transition but it's not working.

---
+++ The-Journey-Of-Sacrifice/Assets/Scripts/SceneTransiion.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransiion : MonoBehaviour
+{
+    public string sceneToLoad;
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    } 
+}
