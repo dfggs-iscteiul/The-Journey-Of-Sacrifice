4ac2c8abbdafb8f7921f890582f21f362abf12f1
Daniel Gonçalves <dfggs@iscte-iul.pt>
2019-12-05 19:12:22 +0000

    JOGO COMPLETO - 1ª VERSÃO
    Adicionado o Menu - Cena Inicial

---
+++ The-Journey-Of-Sacrifice/Assets/Scripts/Play.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+
+    public Animator transitionAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startGame()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        transitionAnim.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(1.5f);
+        var loading = SceneManager.LoadSceneAsync("Story-CS1", LoadSceneMode.Additive);
+        yield return loading;
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("Story-CS1"));
+        SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(0));
+        transitionAnim.SetTrigger("FadeIn");
+    }
+
+}
